d2100362159c50cd70aeb01fe82559669af86832
Mathieu Guillaume <mg@nuxeo.com>
2015-08-05 17:41:20 +0200

    Do not overwrite FT templates

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 
 cd $(dirname $0)
 
-# Make FT templates from release templates
-
-sed 's/Nuxeo_single/NuxeoFT_single/g' templates/Nuxeo.template > templates/NuxeoFT.template
-sed 's/Nuxeo_single/NuxeoFT_single/g' userdata/Nuxeo_single > userdata/NuxeoFT_single
-sed 's/Nuxeo_single/NuxeoFT_single/g' scripts/Nuxeo_single > scripts/NuxeoFT_single
-sed -i -e '/trusty releases/a echo "deb http:\/\/apt.nuxeo.org\/ trusty fasttracks" >> \/etc\/apt\/sources.list.d\/nuxeo.list' scripts/NuxeoFT_single
-
 # Build
 
 if [ ! -d venv ]; then
